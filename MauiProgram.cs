65f64d2a9a9eb610515af3688ab0b1716d296ef3
Yuvraj <11yuviji@gmail.com>
2023-02-27 18:43:51 -0500

    start building calendar

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,28 +1,22 @@
-﻿
-using Microsoft.AspNetCore.Components.WebView.Maui;
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
 using EdUnity.Data;
+using CommunityToolkit.Maui;
 
 namespace EdUnity;
-
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
-
-		builder.Services.AddMauiBlazorWebView();
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+        }).UseMauiCommunityToolkit();
+        builder.Services.AddMauiBlazorWebView();
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
-
-		builder.Services.AddSingleton<WeatherForecastService>();
-
-		return builder.Build();
-	}
+        builder.Services.AddSingleton<WeatherForecastService>();
+        return builder.Build();
+    }
 }
