0e1185bb3caa1388cad61a1046750f8861622a43
Yuvraj <11yuviji@gmail.com>
2023-02-28 21:41:29 -0500

    minor woops

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -5,18 +5,17 @@ using CommunityToolkit.Maui;
 namespace EdUnity;
 public static class MauiProgram
 {
-    public static MauiApp CreateMauiApp()
-    {
-        var builder = MauiApp.CreateBuilder();
-        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
-        {
-            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-        }).UseMauiCommunityToolkit();
-        builder.Services.AddMauiBlazorWebView();
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+		{
+			fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+		}).UseMauiCommunityToolkit();
+		builder.Services.AddMauiBlazorWebView();
 #if DEBUG
-        builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
-        builder.Services.AddSingleton<WeatherForecastService>();
-        return builder.Build();
-    }
+		return builder.Build();
+	}
 }
